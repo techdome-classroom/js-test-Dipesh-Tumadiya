9bfe5dd76b52be9ca103a991cb957e9388df13d0
Dipesh Tumadiya <165694320+Dipesh-Tumadiya@users.noreply.github.com>
2024-05-09 13:23:52 +0530

    Update program2.js
    Done by Dipesh Tumadiya

--- program2.js
+++ program2.js
@@ -1,6 +1,28 @@
 const decodeTheRing = function (s, p) {
 
     // write your code here
+    let encodingMessage="";
+    for(let i=0;i<s.length;i++){
+      let charCode=s.charCodeAt(i);
+      if((charCode>=65 && charCode<=90) || (charCode >=97 && charCode<=122)){
+        let isUpperCase = (charCode >=65 && charCode <=90);
+        let base=isUpperCase ? 65 :97;
+        encodingMessage +=String.fromCharCode(((charCode - base + p)%26)+base);
+      }else if(charCode === 42){
+        encodingMessage += String.fromCharCode(charCode);
+      }else if(charCode === 63){
+        encodingMessage += String.fromCharCode(charCode);
+      }else{
+        encodingMessage += s.charAt(i);
+      }
+    }
+return encodingMessage;
+  };
+
+  function decodingMessage(encodingMessage,p){
+    return encodingMessage(encodingMessage,26 - p);
+
+  };
 
   };
   
